cb320d75418c23d7b202911a7c1232e4f12b3db9
naka <ryousen.naka@microworld.co.jp>
2021-03-26 17:41:45 +0900

    get ok post ng

--- demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,13 +1,14 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
+
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Service
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao{
 
     private static List<Person> DB = new ArrayList<>();
@@ -17,5 +18,10 @@ public class FakePersonDataAccessService implements PersonDao{
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
