1a6f754b9c0ae28730e4f4fa55f12862955581d8
auausen <mayoibito0317@gmail.com>
2021-04-04 22:43:52 +0900

    y

--- 管理（CRUD ）/demo/src/main/java/com/example/demo/controller/EmployeeController.java
+++ 管理（CRUD ）/demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Employee;
 import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -18,5 +21,18 @@ public class EmployeeController {
         return "index";
     }
 
-    public String showNewEmployeeForm
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        //データ作成
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        //従業員データ保存
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
 }
