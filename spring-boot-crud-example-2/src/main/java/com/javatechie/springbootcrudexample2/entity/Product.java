0bb04f6c489fb55a77493edabacb9f96f10eeccc
naka <ryousen.naka@microworld.co.jp>
2021-04-08 17:59:15 +0900

    a

---
+++ spring-boot-crud-example-2/src/main/java/com/javatechie/springbootcrudexample2/entity/Product.java
@@ -0,0 +1,26 @@
+package com.javatechie.springbootcrudexample2.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
