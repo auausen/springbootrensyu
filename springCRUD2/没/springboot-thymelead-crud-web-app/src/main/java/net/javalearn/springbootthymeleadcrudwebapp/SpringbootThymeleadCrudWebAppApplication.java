1f9ec9cfdb22010c0408033a2799db7c42660c5f
naka <ryousen.naka@microworld.co.jp>
2021-04-09 16:35:17 +0900

    ecommercetuika

---
+++ springCRUD2/没/springboot-thymelead-crud-web-app/src/main/java/net/javalearn/springbootthymeleadcrudwebapp/SpringbootThymeleadCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package net.javalearn.springbootthymeleadcrudwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleadCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleadCrudWebAppApplication.class, args);
+	}
+
+}
---
+++ springCRUD2/没/springboot-thymelead-crud-web-app/src/test/java/net/javalearn/springbootthymeleadcrudwebapp/SpringbootThymeleadCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javalearn.springbootthymeleadcrudwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleadCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
